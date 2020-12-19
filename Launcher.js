a803e20189c2cea9056dc129b2190cbe645a1fff
saragupta2008 <74290865+saragupta2008@users.noreply.github.com>
2020-12-19 22:09:09 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,35 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+
+        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA=body
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+
+        if(this.sling.bodyA){
+
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            stroke(0,120,0);
+            line(pointA.x-25,pointA.y,pointB.x-10,pointB.y);
+           
+        }
+    }
+    
+}
--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-var tree1,stone,ground,boy,boyImage;
+const Constraint = Matter.Constraint;
+var tree1,stone,ground,boy,boyImage,mango1,mango2,mango3,mango4,mango5,mango6,launcher;
 
 function preload()
 {
@@ -12,22 +12,29 @@ boyImage=loadImage("boy.png")
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1300, 1200);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
   //Create the Bodies Here.
-  
-   
-   tree1 = new Tree(600,450,350,500);
-   ground = new Ground(600,height,1200,20);
-   stone= new Stone(235,420,30);
-   
-   boy=createSprite(200,100);
+   boy=createSprite(300,1130,20,100);
    boy.addImage(boyImage);
-   boy=scale(0.1);
+   boy.scale=0.1;
+   
+   tree1  = new Tree(1100,930,350,500);
+   ground = new Ground(650,height,1300,20);
+   stone  = new Stone(260,1070,30);
+
+   mango1 = new Mango(1100,850,40);
+   mango2 = new Mango(1060,800,38);
+   mango3 = new Mango(1130,780,36);
+   mango4 = new Mango(980,900,30);
+   mango5 = new Mango(1200,890,40);
+   mango6 = new Mango(1190,780,35);
+   launcher = new Launcher(stone.body,{x:260, y:1070});
+   
    
 	Engine.run(engine);
   
@@ -40,13 +47,51 @@ function draw() {
   
  
  tree1.display();
- stone.display();
- ground.display();
  
+ ground.display();
+ mango1.display();
+ mango2.display();
+ mango3.display();
+ mango4.display();
+ mango5.display();
+ mango6.display();
+ stone.display();
+ launcher.display();
+
+ detectCollision(stone,mango1);
+ detectCollision(stone,mango2);
+ detectCollision(stone,mango3);
+ detectCollision(stone,mango4);
+ detectCollision(stone,mango5);
+ detectCollision(stone,mango6);
 
   drawSprites();
  
 }
 
+function mouseDragged(){
+  Matter.Body.setPosition(stone.body, {x: mouseX , y: mouseY});
+}
 
+
+function mouseReleased(){
+  launcher.fly();
+}
+
+function detectCollision(stone,mango){
+mangoBodyPosition=mango.body.position 
+stoneBodyPosition=stone.body.position
+
+var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y) 
+if(distance<=mango.r+stone.r)
+{
+Matter.Body.setStatic(mango.body,false);
+}
+}
+function keyPressed(){
+  if(keyCode==32){
+    Matter.Body.setPosition(stone.body,{x:260,y:1070})
+      launcher.attach(stone.body);
+  }
+}
 
